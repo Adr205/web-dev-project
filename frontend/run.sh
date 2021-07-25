644a4c801a618951eb25ae6409ec5dcc1678261a
albonicc <nikygam@gmail.com>
2021-07-24 19:01:27 -0500

    Created different folders for frontend and backend

---
+++ frontend/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# sudo docker build -t frontend:1.0 .
+
+sudo docker run \
+    -it \
+    --rm \
+    -v xdx:/app \
+    -p 3000:3000 \
+    -e CHOKIDAR_USEPOLLING=true \
+    frontend:1.0
