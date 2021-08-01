73eaf7711d69a65f5e082c5b94489d8ea3261499
albonicc <nikygam@gmail.com>
2021-07-31 22:38:18 -0500

    Now GameInfo succesfully fetches data with game details

--- frontend/src/components/Info.jsx
+++ frontend/src/components/Info.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 
 
-const Info = ({ game, image }) => {
+const Info = ({ name, background_image, description, released }) => {
 
     return (
         <div className="game-info-container">
             <div className="game-left">
-                <h1>{game.name}</h1>
-                <img src={game.background_image} alt={game.name} />
+                <h1>{name}</h1>
+                <img src={background_image} alt={name} />
             </div>
             <div className="game-center">
-                <p className="description">{game.description}</p>
-                <p className="date">Released Date: {game.released}</p>
+                <p className="description">{description}</p>
+                <p className="date">Released Date: {released}</p>
             </div>
             <div className="game-photos-ingame">
-            <img src={image[0].image} alt="" />
-            <img src={image[1].image} alt="" />
+            {/* <img src={image[0].image} alt="" />
+            <img src={image[1].image} alt="" /> */}
             </div>
-
         </div>
     )
 }
--- frontend/src/containers/GameInfo.jsx
+++ frontend/src/containers/GameInfo.jsx
@@ -2,57 +2,66 @@ import React, { useEffect, useState } from 'react';
 import '../assets/styles/GameInfo.css';
 import Info from "../components/Info";
 const config = require("../config");
-const UGS = config.api.UniqueGameSearch;
-const KEY = config.api.API_Key;
+// const UGS = config.api.UniqueGameSearch;
+// const KEY = config.api.API_Key;
+const API_URL = config.api.API_URL;
+const API_KEY = config.api.API_KEY;
 
 
 
-const GameInfo = ({id}) => {
-    const link = UGS + id + KEY;
-    const lPhotos = UGS + id +"/screenshots" + KEY;
+const GameInfo = ({ id }) => {
+    const API = `${API_URL}/${id}${API_KEY}`;
+    const [gameInfo, setGameInfo] = useState(null);
+    // const link = UGS + id + KEY;
+    // const lPhotos = UGS + id +"/screenshots" + KEY;
     //console.log(link);    
-    const [info, setInfo] = useState([]);
-    const [photos, setPhotos] = useState([]);
-    var props = {};
+    // const [photos, setPhotos] = useState([]);
+    // var props = {};
 
-    const getGame = (API) => {
-         fetch(API)
-            .then((res) => res.json())
-            .then((data) => {
-                setInfo(data);
-                //console.log(data.name);
-            })
+    const getGame = async (API) => {
+        // fetch(API)
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //         setInfo(data);
+        //         //console.log(data.name);
+        //     })
+        const response = await fetch(API);
+        const gameInfo = await response.json();
+        setGameInfo(gameInfo)
+        // console.log(gameInfo)
     }
 
-    const getPhotos = (API) => {
-         fetch(API)
-            .then((res) => res.json())
-            .then((data) => {
-                 setPhotos(data.results);
-                // console.log(data);
-            })
-    }
+    // const getPhotos = (API) => {
+    //      fetch(API)
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //              setPhotos(data.results);
+    //             // console.log(data);
+    //         })
+    // }
 
-    useEffect(  () => {
-            getPhotos(lPhotos);
-            getGame(link);
-    },[]);
-    
-     setProps(photos,info);       
-    
+    useEffect(() => {
+        // getPhotos(lPhotos);
+        getGame(API);
+    }, []);
 
-     function setProps(photos,info){
-        props =  {
-            game : info,
-            image : photos
-        }
-        console.log(props)
-    }
+    // setProps(photos, info);
+
+
+    // function setProps(photos, info) {
+    //     props = {
+    //         game: info,
+    //         image: photos
+    //     }
+    //     console.log(props)
+    // }
 
     // console.log(props);
-     return(
+    return (
         <>
-        <Info {...props}/>
+            {/* <Info {...props} /> */}
+            {/* {console.log(gameInfo)} */}
+            <Info {...gameInfo} />
         </>
     )
 }
