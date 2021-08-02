f87c0234aab90808ee85b2e0c88e9f152798962f
albonicc <nikygam@gmail.com>
2021-08-02 11:19:04 -0500

    Now comments are loaded for each game

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -21,21 +21,32 @@ const Comment = ({ id }) => {
         <>
             {
                 comments.map((comment) => {
-                    return (
-                        <div className="review-container">
-                            <div className="review-header">
-                                <h3 className="review-title">{comment.title}</h3>
-                                <h4 className="review-user"> {comment.author} <span className="review-date">29 July 2021</span> </h4>
+                    if (comment.gameId === id) {
+                        return (
+                            <div className="review-container">
+                                <div className="review-header">
+                                    <h3 className="review-title">{comment.title}</h3>
+                                    <h4 className="review-user"> {comment.author} <span className="review-date">29 July 2021</span> </h4>
+                                </div>
+                                <div className="review-body">
+                                    <p className="review-body-text">{comment.text}</p>
+                                </div>
                             </div>
-                            <div className="review-body">
-                                <p className="review-body-text">{comment.text}</p>
-                            </div>
-                        </div>
-                    )
+                        )
+                    }
                     // console.log(comment.text);
                 })
             }
-            {console.log(comments)}
+
+            {/* {console.log(comments.length)} */}
+
+            {/* {
+                comments.length < 1 ?
+                    <div className="review-container">
+                        <h2>Be the first to post a review for this game!</h2>
+                    </div> :
+                    null
+            } */}
         </>
     )
 }
